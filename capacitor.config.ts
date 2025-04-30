214ba79ffa6497792273d8304ec52da2117a165d
fernando.reta <freta@finmancorp.com>
2025-04-30 16:10:38 -0500

    capacitor ios and android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.werttdev.perksy',
+  appName: 'perksy',
+  webDir: 'dist/perksy/browser'
+};
+
+export default config;
